7d5717a841d39633f8cfa00318dd3e6b8ddd837f
Raul Lendines <91599628+raullendines@users.noreply.github.com>
2023-01-13 11:30:07 +0100

    Finish

--- src/MainServer.java
+++ src/MainServer.java
@@ -1,20 +1,31 @@
-import java.io.DataInputStream;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class MainServer {
+    static ServerSocket serverSocket;
+    static Socket clientSocket;
+
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(SocketsControllers.PORT);
+        serverSocket = new ServerSocket(SocketsControllers.PORT);
         System.out.println("Esperando para la conexion en el puerto " + serverSocket.getLocalPort());
         while (true) {
-            Socket clientSocket = serverSocket.accept();
+            clientSocket = serverSocket.accept();
+            showFiles();
             System.out.println("Conectado a " + clientSocket.getInetAddress());
             SocketsControllers sC = new SocketsControllers(clientSocket);
             sC.actionServer();
             clientSocket.close();
         }
+    }
+    public static void showFiles() throws IOException {
+        File folder = new File(SocketsControllers.dirBase);
+        File[] files = folder.listFiles();
+
+        ObjectOutputStream oos = new ObjectOutputStream(clientSocket.getOutputStream());
+
+        oos.writeObject(files);
     }
 }
